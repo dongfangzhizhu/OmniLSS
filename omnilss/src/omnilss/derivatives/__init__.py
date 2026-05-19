2cd2907377c91835f73deb80b3b13ef548fd1983
dongfangzhizhu <axu0606@gmail.com>
2026-05-20 06:12:50 +0800

    feat(cg): add week1 cross-derivative infrastructure and tests

---
+++ omnilss/src/omnilss/derivatives/__init__.py
@@ -0,0 +1,5 @@
+"""Derivative helper modules for algorithm development."""
+
+from .cross_derivatives import cross_hessian, cross_hessian_from_family
+
+__all__ = ["cross_hessian", "cross_hessian_from_family"]
